9a0098c78f2a9739b0ebcda21662198e44f6df5f
ob.raza <ob.raza3756@gmail.com>
2024-06-13 15:17:27 +0200

    Add test.py file

---
+++ test.py
@@ -0,0 +1,9 @@
+# simple_script.py
+import time
+
+# A simple loop that prints numbers from 1 to 10 with a delay
+for i in range(1, 11):
+    print(f"Number: {i}")
+    time.sleep(1)
+
+print("Program End")
